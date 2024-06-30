922fecd6fd69ab28b791ebc945f7784404523d3b
zidoopal2@gmail.com <zidoopal2@gmail.com>
2024-07-01 02:04:30 +0900

    Init: Initial file settings

---
+++ js/data.js
@@ -0,0 +1,38 @@
+export const photoData = [
+  {
+    src: './assets/img01.png',
+    alt: '제주 귤나무',
+  },
+  {
+    src: './assets/img02.png',
+    alt: '뜨개 지갑',
+  },
+  {
+    src: './assets/img03.png',
+    alt: '제주도 오름 정상에 서 있는 두팔',
+  },
+  {
+    src: './assets/img04.png',
+    alt: '카페에서 찍은 커피',
+  },
+  {
+    src: './assets/img05.png',
+    alt: '카페에서 찍은 베이커리 와 커피',
+  },
+  {
+    src: './assets/img06.png',
+    alt: '전주에서 찍은 두팔이의 뒷모습',
+  },
+  {
+    src: './assets/img07.png',
+    alt: '과일 모양 뜨개 지갑',
+  },
+  {
+    src: './assets/img08.png',
+    alt: '코바늘로 뜬 비니',
+  },
+  {
+    src: './assets/img09.png',
+    alt: '탁구 인증 사진',
+  },
+];
